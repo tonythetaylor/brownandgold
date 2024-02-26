cffe10c9f913dbc79a86b4cf95094e15a35e22e4
Anthony Taylor <ant.taylor2@gmail.com>
2024-02-26 00:30:22 -0500

    testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
